b6f003c1d4edd8916b50db1b741c8cb7e3a40300
Andrew Wagner <andrew9010.av@gmail.com>
2022-03-25 16:33:41 +0600

    Initial commit

---
+++ test_lock_constr/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test_lock_constr
+{
+    static class Program
+    {
+        private static object _lockObj = new object();
+        private static int lock_int = new int();
+        static void Main(string[] args)
+        {
+            lock ((lock_int as object))
+            {
+                Console.WriteLine("Test lock");
+            }
+        }
+    }
+}
